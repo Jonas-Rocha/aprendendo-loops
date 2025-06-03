614374b224301a6c5e512dcee0717ca97ea44519
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-06-03 12:39:04 -0300

    Loop for

--- script.js
+++ script.js
@@ -42,3 +42,38 @@ do {
 } while (value2 < 10);
 
 console.log("Segue o fluxo...");
+
+/*
+FOR repete ENQUANTO a condição for VERDADEIRA.
+
+- variável de controle
+- condição
+- incremento ou decremento da variável de controle
+*/
+
+for (step = 0; step < 10; step++) {
+  console.log(step);
+}
+
+// Exemplo de tabuada.
+console.log("### TABUADA ###");
+console.log(`7x0 = ${7 * 0}`);
+console.log(`7x1 = ${7 * 1}`);
+console.log(`7x2 = ${7 * 2}`);
+console.log(`7x3 = ${7 * 3}`);
+console.log(`7x4 = ${7 * 4}`);
+console.log(`7x5 = ${7 * 5}`);
+console.log(`7x6 = ${7 * 6}`);
+console.log(`7x7 = ${7 * 7}`);
+console.log(`7x8 = ${7 * 8}`);
+console.log(`7x9 = ${7 * 9}`);
+console.log(`7x10 = ${7 * 10}`);
+
+// Fazendo o mesmo exemplo acima utiliando o for()
+
+console.log("@@@@ TABUADA COM O FOR @@@@");
+let number = 7;
+
+for (step = 0; step <= 10; step++) {
+  console.log(`${number} x ${step} = ${number * step}`);
+}
